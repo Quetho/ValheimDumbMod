14ee13004122b3a9c8638fc66f03ba118bc82200
quetho <q.thomas@live.com>
2026-09-25 17:57:55 +0200

    Qmod 1.0.84

--- Qmod/Core/Qmod.cs
+++ Qmod/Core/Qmod.cs
@@ -12,7 +12,7 @@ namespace Qmod
     {
         public const string PluginGUID = "com.aeons.qmod";
         public const string PluginName = "Qmod";
-        public const string PluginVersion = "1.0.83";
+        public const string PluginVersion = "1.0.84";
 
         internal static Qmod Instance { get; private set; }
 
